4777c14ae6c1bef1809d15ca2465ca3d29d62e4e
José Neto <joseluziadsneto@gmail.com>
2023-04-22 15:27:15 -0300

    Add gitignore
    Add gitignore no respositorio

---
+++ C++/URI_1003.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int A, B;
+
+    int soma;
+
+    cin>>A>>B;
+
+    soma = A + B;
+
+    cout<<"SOMA = "<<soma<<endl;
+
+
+
+    return 0;
+}
